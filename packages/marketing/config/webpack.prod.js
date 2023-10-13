5e84ffc5d7a8f6c647a212bb254de0b8febb713a
SauravPlus <saurav.rawat@simpplr.com>
2023-10-13 12:49:05 +0530

    added marketing deployement

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,6 +8,10 @@ const prodConfig = {
   output: {
     // this will make sure whenever we are building files for production it will use the bellow template
     filename: "[name].[contenthash].js",
+    // public path option will be used whenever some part of webpack trying to refer to some part of file built by webpack. previously html webpack plugin
+    // was trying to refer directly file name i.e(cloudFrontUrl/filename) but in aws s3 we have folder structure /container/latest/ for our mfe so public
+    // path will prepend while creating file name and we'll get correct public path
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
